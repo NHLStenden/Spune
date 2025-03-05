f03467947e5976318bdbfb22c501e10ecb949ee3
martin.bosgra <martin.bosgra@nhlstenden.com>
2025-03-05 07:20:31 +0100

    Small qc fixes

--- Spune.Core/Functions/PlaceholderFunction.cs
+++ Spune.Core/Functions/PlaceholderFunction.cs
@@ -24,22 +24,7 @@ public static class PlaceholderFunction
     {
         return ReplacePlaceholders(text, dictionary, string.Empty);
     }
-
-    /// <summary>
-    /// Replaces placeholders in a string with corresponding values from a dictionary.
-    /// </summary>
-    /// <param name="text">The string containing placeholders.</param>
-    /// <param name="dictionary">A dictionary with keys as placeholder names and values as replacement values.</param>
-    /// <param name="keyPrefix">A prefix for the key identifier.</param>
-    /// <returns>The string with placeholders replaced by their corresponding values.</returns>
-    public static string ReplacePlaceholders(string text, IDictionary<string, List<string>> dictionary, string keyPrefix)
-    {
-        var newDictionary = new Dictionary<string, string>();
-        foreach (var (key, value) in dictionary)
-            newDictionary[key] = StringFunction.StringsToString(value, ", ");
-        return ReplacePlaceholders(text, newDictionary, keyPrefix);
-    }
-
+    
     /// <summary>
     /// Replaces placeholders in a string with corresponding values from a dictionary.
     /// </summary>
@@ -67,5 +52,20 @@ public static class PlaceholderFunction
         result = result.Replace(doubleRightBrace, "}}", StringComparison.Ordinal);
 
         return result;
-    }    
+    }
+
+    /// <summary>
+    /// Replaces placeholders in a string with corresponding values from a dictionary.
+    /// </summary>
+    /// <param name="text">The string containing placeholders.</param>
+    /// <param name="dictionary">A dictionary with keys as placeholder names and values as replacement values.</param>
+    /// <param name="keyPrefix">A prefix for the key identifier.</param>
+    /// <returns>The string with placeholders replaced by their corresponding values.</returns>
+    static string ReplacePlaceholders(string text, IDictionary<string, List<string>> dictionary, string keyPrefix)
+    {
+        var newDictionary = new Dictionary<string, string>();
+        foreach (var (key, value) in dictionary)
+            newDictionary[key] = StringFunction.StringsToString(value, ", ");
+        return ReplacePlaceholders(text, newDictionary, keyPrefix);
+    }
 }
